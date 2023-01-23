d66339a3336ee3559da4c2e9fa909aea7c8e8a4e
ismailkaya83 <tor232191@hotmail.com>
2023-01-23 21:30:58 +0100

    installed required jquery codes

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
+
 
 app.use('/', indexRouter);
 // app.use('/highlights', highlightsRouter);
