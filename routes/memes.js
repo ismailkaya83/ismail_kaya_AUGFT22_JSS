372d13adc578f78646f1cfa591b37341e59c4c47
ismailkaya83 <tor232191@hotmail.com>
2023-01-24 20:50:04 +0100

    Meme Overview page

--- routes/memes.js
+++ routes/memes.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // const { resolve } = require('path');
-const axios = require('axios');
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
-//BM New API endpoint to use for the data from the soccer API
-router.get('/', function (req, res, next) {});
-
+let memes = {};
+router.get("/", function (req, res, next) {
+  axios
+    .get(process.env.API_URL)
+    .then((response) => {
+      memes = response.data.data.memes;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  res.render("memes", { memes: memes });
+});
 module.exports = router;
+
+
+
