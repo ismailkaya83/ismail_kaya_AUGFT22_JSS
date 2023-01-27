4ba3e0224a37de6d0156fe77ea3eccc29f74daf9
ismailkaya83 <ismailkaya83@yahoo.com>
2023-01-27 21:33:31 +0100

    meme details page

--- routes/meme.js
+++ routes/meme.js
@@ -1,8 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-// const { resolve } = require('path');
-const axios = require('axios');
 
-router.get('/', function (req, res, next) {});
+router.post("/", function (req, res, next) {
+    try {
+        const {id} = req.body;
+        const meme = memes.find((meme) => meme.id === id);
+        console.log("MEME", meme);
+        res.render("meme", {meme});
+        res.status(200).json({message: "success"});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "server error"});
+    }
+});
 
 module.exports = router;
--- routes/memes.js
+++ routes/memes.js
@@ -5,19 +5,21 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 
-let memes = {};
+global.memes = [];
 router.get("/", function (req, res, next) {
-  axios
-    .get(process.env.API_URL)
-    .then((response) => {
-      memes = response.data.data.memes;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  res.render("memes", { memes: memes });
+    axios
+        .get(process.env.API_URL)
+        .then((response) => {
+            global.memes = response.data.data.memes;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    res.render("memes", {memes});
 });
+
 module.exports = router;
+
 
 
 
