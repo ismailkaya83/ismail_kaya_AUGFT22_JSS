2d4e25a9124f9975deca547d66325381c2aed313
ismailkaya83 <ismailkaya83@yahoo.com>
2023-01-27 21:32:02 +0100

    install body parser

--- app.js
+++ app.js
@@ -3,20 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 var indexRouter = require('./routes/index');
 // var highlightsRouter = require('./routes/highlights');
 var memeRouter = require('./routes/meme');
 var memesRouter = require('./routes/memes');
 
-
-
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use('/', indexRouter);
+// app.use('/highlights', highlightsRouter);
+app.use('/meme', memeRouter);
+app.use('/memes', memesRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,16 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
-
-
-app.use('/', indexRouter);
-// app.use('/highlights', highlightsRouter);
-app.use('/meme', memeRouter);
-app.use('/memes', memesRouter);
-
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
